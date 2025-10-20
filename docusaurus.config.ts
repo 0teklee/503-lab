9475a802582a848592d64acb610353fe25b55997
0teklee <leetekwoo@gmail.com>
2025-10-20 16:40:08 +0900

    init-docu

---
+++ docusaurus.config.ts
@@ -0,0 +1,131 @@
+import {themes as prismThemes} from "prism-react-renderer";
+import type {Config} from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+	title: "503",
+	tagline: "Techup CybrSec Study Records",
+	favicon: "img/favicon.ico",
+
+	// Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+	future: {
+		v4: true, // Improve compatibility with the upcoming Docusaurus v4
+	},
+
+	// Set the production url of your site here
+	url: "https://study.com",
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: "/",
+
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "503", // Usually your GitHub org/user name.
+	projectName: "503-lab", // Usually your repo name.
+
+	onBrokenLinks: "throw",
+
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en"],
+	},
+
+	presets: [
+		[
+			"classic",
+			{
+				docs: {
+					sidebarPath: "./sidebars.ts",
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+				},
+				blog: {
+					showReadingTime: true,
+					feedOptions: {
+						type: ["rss", "atom"],
+						xslt: true,
+					},
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+					// Useful options to enforce blogging best practices
+					onInlineTags: "warn",
+					onInlineAuthors: "warn",
+					onUntruncatedBlogPosts: "warn",
+				},
+				theme: {
+					customCss: "./src/css/custom.css",
+				},
+			} satisfies Preset.Options,
+		],
+	],
+
+	themeConfig: {
+		// Replace with your project's social card
+		image: "logo.png",
+		colorMode: {
+			respectPrefersColorScheme: false,
+			defaultMode: "dark",
+			disableSwitch: true,
+		},
+		navbar: {
+			title: "503-lab",
+			logo: {
+				alt: "503 스터디그룹 자료 모음집",
+				src: "img/logo.svg",
+			},
+			items: [
+				{
+					type: "docSidebar",
+					sidebarId: "tutorialSidebar",
+					position: "left",
+					label: "Tutorial",
+				},
+				{to: "/blog", label: "Blog", position: "left"},
+				{
+					href: "https://github.com/",
+					label: "GitHub",
+					position: "right",
+				},
+			],
+		},
+		footer: {
+			style: "dark",
+			links: [
+				{
+					title: "Authors",
+					items: [
+						{
+							label: "TBA",
+							to: "/docs/intro",
+						},
+					],
+				},
+				{
+					title: "Github",
+					items: [
+						{
+							label: "GitHub",
+							href: "https://github.com/0teklee/503-lab",
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} 503, Inc. Built with Docusaurus.`,
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
+};
+
+export default config;
